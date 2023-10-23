781fcc12cae2146ba34554e16c5cbfe0ca65cabe
PIW <loveelf1@naver.com>
2023-10-23 18:23:34 +0900

    DiscountPolicy 객체 인터페이스화 및 구현체(NoneDiscountPolicy, DefaultDiscountPolicy) 분리

--- src/main/kotlin/movie_reservation_application/discount/AmountDiscountPolicy.kt
+++ src/main/kotlin/movie_reservation_application/discount/AmountDiscountPolicy.kt
@@ -7,10 +7,10 @@ import movie_reservation_application.Screening
  * DiscountPolicy의 자식 클래스
  * 할인 조건을 만족할 경우 일정한 금액을 할인해주는 금액 할인 정책을 구현한다.
  */
-class AmountDiscountPolicy(
+class AmountDefaultDiscountPolicy(
     private var discountAmount: Money,
     vararg conditions: DiscountCondition
-) : DiscountPolicy(*conditions) {
+) : DefaultDiscountPolicy(*conditions) {
     /**
      * getDiscountAmount 함수를 오버라이딩하여 구현한다.
      * 할인금액을 Return
--- src/main/kotlin/movie_reservation_application/discount/PercentDiscountPolicy.kt
+++ src/main/kotlin/movie_reservation_application/discount/PercentDiscountPolicy.kt
@@ -7,10 +7,10 @@ import movie_reservation_application.Screening
  * DiscountPolicy의 자식 클래스
  * 할인 조건을 만족할 경우 일정한 비율을 할인해주는 금액 할인 정책을 구현한다.
  */
-class PercentDiscountPolicy(
+class PercentDefaultDiscountPolicy(
     private val percent: Double,
     vararg conditions: DiscountCondition
-) : DiscountPolicy(*conditions){
+) : DefaultDiscountPolicy(*conditions){
     /**
      * getDiscountAmount 함수를 오버라이딩하여 구현한다.
      * 할인 비율을 Return
